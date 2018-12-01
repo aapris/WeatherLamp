c596d5fc428038dac0d7e8eb926384573cddd203
Aapo Rista <aapo.gitpub@rista.net>
2018-12-01 22:19:47 +0200

    Implement room functionality

--- pikkujoulu_ui/pikkujoulu_ui.py
+++ pikkujoulu_ui/pikkujoulu_ui.py
@@ -10,19 +10,33 @@ written with Flask.
 """
 
 import json
-import paho.mqtt.client as mqtt
-from flask import (Flask, Response, render_template)
+import os
+import random
+
+from flask import (Flask, Response, request, redirect, url_for, render_template, send_from_directory)
 from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room, rooms
 from flask_mqtt import Mqtt
 from flask_bootstrap import Bootstrap
 import eventlet
 
+
+# Colorful error and debug prints
+def print_error(skk):
+    print("\033[91m {}\033[00m".format(skk))
+
+
+def debug(skk):
+    if DEBUG:
+        print("\033[92m {}\033[00m".format(skk))
+
+
 try:
     from .pikkujoulu_ui_config import MQTT_HOST, MQTT_PORT, MQTT_USER, MQTT_PASS, SECRET_KEY
 except ImportError as err:
-    print('\n\nNo pikkujoulu_ui_config found. Did you remember to')
-    print('    cp pikkujoulu_ui_config_example.py pikkujoulu_ui_config.py')
-    print('and set the variables correctly?\n\n\n')
+    print_error('\n\nNo pikkujoulu_ui_config found. Did you remember to')
+    print_error('    cp pikkujoulu_ui_config_example.py pikkujoulu_ui_config.py')
+    print_error('and set the variables correctly?\n\n\n')
     raise
 
 # configuration
@@ -49,7 +63,7 @@ bootstrap = Bootstrap(app)
 
 # MQTT stuff
 topic = 'led/#'
-topic2 = 'led/control'
+topic2 = 'led/ctrl'
 port = 1883
 
 
@@ -66,43 +80,60 @@ def get_mqtt_data(message):
 @mqtt.on_connect()
 def handle_connect(client, userdata, flags, rc):
     mqtt.subscribe(topic)
-    mqtt.publish('led/server', b'hello world')
-    print("MQTT connected")
+    mqtt.publish('led/server', b'Hello world, a server has started.')
+    debug("MQTT connected")
 
 
 @mqtt.on_message()
 def handle_mqtt_message(client, userdata, message):
-    print("ON MQTT MESSAGE")
     data = get_mqtt_data(message)
     e = 'debug'
     data['type'] = e
     t = data['topic'].split('/')
     if data['topic'].startswith('led/ping/'):
-        print("Got message with ping topic")
+        debug("Got MQTT message with ping topic")
+        pass
+    room = 'NoRoom'
     if len(t) > 1 and t[1] == 'ping':
         e = 'ping'
-        data['dev'] = t[2]
+        data['dev'] = t[-1]
         data['type'] = e
-    socketio.emit(e, data=data)
+    if len(t) == 4:
+        room = t[2]
+    if room == 'NoRoom':
+        socketio.emit("debug", data=data)
+        return
+    socketio.emit(e, data=data, room=room)
 
 
 # Web socket stuff
-@socketio.on('message')
-def handle_message(message):
-    print('SOCKETIO received message: ' + message)
+
+@socketio.on('connect')
+def handle_connection_event():
+    room = request.args.get('roomName')  # defined in client's io.connect query parameter
+    join_room(room)
+    debug('Client connected and joined to room {}'.format(room))
+    data = {"info": "Connection ok, joined to {}".format(room)}
+    socketio.emit("debug", data=data, room=room)
 
 
 @socketio.on('ledclick')
 def handle_ledclick_event(json):
-    print('received json: ' + str(json))
-    topic = 'led/control/{}'.format(json['dev'])
-    msg = b'002\xff\x00\x00'
+    debug('received json: ' + str(json))
+    client_rooms = [x for x in rooms() if len(x) < 30]
+    if len(client_rooms) == 1:
+        room = client_rooms[0]
+    else:
+        room = 'ERROR_IN_ROOM'
+    topic = '{}/{}/{}'.format(topic2, room, json['dev'])
+    random_mode = '{}'.format(random.randint(0, 5)).encode()  # returns byte '0'..'5'
+    msg = b'00' + random_mode + b'\xff\x00\x00'
     mqtt.publish(topic, msg)
 
 
 @socketio.on('buttonclick')
 def handle_my_custom_event(json):
-    print('received json: ' + str(json))
+    debug('received json: ' + str(json))
 
 
 @app.route('/')
@@ -111,12 +142,15 @@ def index():
     return render_template('index.html', messages=modes)
 
 
-@app.route('/modes')
-def modes():
-    json_modes = []
-    json_modes.append({
-        'id': '1',
-        'url': '/foo/bar',
-    })
-    json_str = json.dumps(json_modes, indent=1)
-    return Response(response=json_str, content_type='application/json')
+@app.route('/room/<path:roomname>')
+def room(roomname):
+    if len(roomname) > 20:
+        return redirect(url_for('room', roomname=roomname[:20]))
+    modes = []
+    return render_template('index.html', messages=modes, roomname=roomname)
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico',
+                               mimetype='image/vnd.microsoft.icon')
--- pikkujoulu_ui/static/main.js
+++ pikkujoulu_ui/static/main.js
@@ -4,7 +4,7 @@ function updateLastSeen() {
         var age = milliseconds - $(this).data('lastseen');
         var age_str = Number.parseFloat(age/1000).toFixed(1);
         $(this).find('.uptime').text('Seen: ' + age_str + ' sec ago');
-        if (age > 10000) {$(this).addClass("outdated");}
+        if (age > 15000) {$(this).addClass("outdated");}
         // console.log("AGE " + age);
     });
 }
@@ -19,14 +19,22 @@ function highlight(element_id){
 
 
 var leds = {};
-var socket = io.connect('http://' + document.domain + ':' + location.port);
-socket.on('connect', function () {
-    socket.emit('my event', {data: 'CONNECTAATIO ON TAPAHTUNUT'});
-    $('#msg').append("CONNECTED!<br>");
+//var socket = io.connect('http://' + document.domain + ':' + location.port);
+var roomName = 'PikkuJoulu';
+
+var socket = io('http://' + document.domain + ':' + location.port, {
+    query: {roomName: roomName,},
 });
+
+socket.on('connect', function () {
+    socket.emit('room', roomName);
+    socket.emit('connection', {data: 'Hello from web client'});
+    $('#connected').text("CONNECTED!");
+});
+
 socket.on('debug', function (data) {
     if (data === void (0)) {
-        console.log("Got undefined data");
+        // console.log("Got undefined data");
         return;
     }
     console.log("DEBUG " + data.type);
@@ -35,21 +43,21 @@ socket.on('debug', function (data) {
 
 socket.on('ping', function (data) {
     if (data === void (0)) {
-        console.log("Got undefined data");
+        // console.log("Got undefined data");
         return;
     }
-    console.log("PING " + data.type);
-    console.log(data);
+    // console.log("PING " + data.type);
+    // console.log(data);
     var milliseconds = (new Date).getTime();
     var _id = data.dev;  // DIV's id value
     if (data.dev in leds) {
-        console.log("Led device exists: " + data.dev);
+        // console.log("Led device exists: " + data.dev);
     } else {
         console.log("New led device: " + data.dev);
         $('#msg').append("<div class='led' id='" + _id + "'>" + data.dev + "<br><span class='uptime'>Seen: 0.0 sec ago</span></div>");
         leds[data.dev] = 1;
     }
-    console.log(leds);
+    // console.log(leds);
     var leddiv = $('#' + _id);
     leddiv.data('lastseen', milliseconds);
     leddiv.removeClass("outdated");
