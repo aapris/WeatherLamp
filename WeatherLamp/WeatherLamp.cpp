c10fbde6368089ab0c338eb5d7394b9534b9330d
Aapo Rista <aapo.gitpub@rista.net>
2020-11-02 00:09:30 +0200

    Pattern from server works now

--- WeatherLamp/WeatherLamp.cpp
+++ WeatherLamp/WeatherLamp.cpp
@@ -65,7 +65,6 @@ char controlTopicBrc[50];
 void requestData();
 void requestData2();
 void runLedEffect();
-void Fire2012();
 
 void setup()
 {
@@ -113,7 +112,7 @@ void requestData()
   if (WiFi.status() == WL_CONNECTED)
   {
     HTTPClient http;
-    String http_url = "http://rista.net/";
+    String http_url = "http://porr.rista.fi/weatherlamp.bin";
     String serverPath = http_url + "?temperature=24.37";
 
     // Your Domain name with URL path or IP address with path
@@ -128,6 +127,23 @@ void requestData()
       Serial.println(httpResponseCode);
       String payload = http.getString();
       Serial.println(payload);
+      currentPalette = CRGBPalette16();
+
+      for (int i = 0; i < 16; i++) {
+        uint8_t i1 = i*3;
+        uint8_t i2 = i*3 + 1;
+        uint8_t i3 = i*3 + 2;
+        uint8_t r = (uint8_t)payload[i1];
+        uint8_t g = (uint8_t)payload[i2];
+        uint8_t b = (uint8_t)payload[i3];
+        Serial.print(r);
+        Serial.print(",");
+        Serial.print(g);
+        Serial.print(",");
+        Serial.print(b);
+        Serial.println();
+        currentPalette[i] = CRGB(r, g, b);
+      }
     }
     else
     {
@@ -235,7 +251,7 @@ void FillLEDsFromPaletteColors(uint8_t colorIndex)
   for (int i = 0; i < NUM_LEDS; i++)
   {
     leds[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
-    colorIndex += 3;
+    colorIndex += 8;
   }
 }
 
@@ -247,120 +263,19 @@ void FillLEDsWithSolidColor()
   }
 }
 
-void runActiveEffect()
-{
-  switch (activeEffect)
-  {
-  case '0':
-    // Serial.println("Switch to Cylon");
-    Fire2012(); // run simulation frame
-    break;
-  case '1':
-    // Serial.println("Switch to Fire");
-    Fire2012(); // run simulation frame
-    break;
-  default:
-    // Serial.print("Invalid effect: ");
-    // Serial.println(activeEffect);
-    break;
-  }
-}
-
-bool gReverseDirection = false;
-
-// Fire2012 by Mark Kriegsman, July 2012
-// as part of "Five Elements" shown here: http://youtu.be/knWiGsmgycY
-////
-// This basic one-dimensional 'fire' simulation works roughly as follows:
-// There's a underlying array of 'heat' cells, that model the temperature
-// at each point along the line.  Every cycle through the simulation,
-// four steps are performed:
-//  1) All cells cool down a little bit, losing heat to the air
-//  2) The heat from each cell drifts 'up' and diffuses a little
-//  3) Sometimes randomly new 'sparks' of heat are added at the bottom
-//  4) The heat from each cell is rendered as a color into the leds array
-//     The heat-to-color mapping uses a black-body radiation approximation.
-//
-// Temperature is in arbitrary units from 0 (cold black) to 255 (white hot).
-//
-// This simulation scales it self a bit depending on NUM_LEDS; it should look
-// "OK" on anywhere from 20 to 100 LEDs without too much tweaking.
-//
-// I recommend running this simulation at anywhere from 30-100 frames per second,
-// meaning an interframe delay of about 10-35 milliseconds.
-//
-// Looks best on a high-density LED setup (60+ pixels/meter).
-//
-//
-// There are two main parameters you can play with to control the look and
-// feel of your fire: COOLING (used in step 1 above), and SPARKING (used
-// in step 3 above).
-//
-// COOLING: How much does the air cool as it rises?
-// Less cooling = taller flames.  More cooling = shorter flames.
-// Default 50, suggested range 20-100
-#define COOLING 55
-
-// SPARKING: What chance (out of 255) is there that a new spark will be lit?
-// Higher chance = more roaring fire.  Lower chance = more flickery fire.
-// Default 120, suggested range 50-200.
-#define SPARKING 120
-
-void Fire2012()
-{
-  // Array of temperature readings at each simulation cell
-  static byte heat[NUM_LEDS];
-
-  // Step 1.  Cool down every cell a little
-  for (int i = 0; i < NUM_LEDS; i++)
-  {
-    heat[i] = qsub8(heat[i], random8(0, ((COOLING * 10) / NUM_LEDS) + 2));
-  }
-
-  // Step 2.  Heat from each cell drifts 'up' and diffuses a little
-  for (int k = NUM_LEDS - 1; k >= 2; k--)
-  {
-    heat[k] = (heat[k - 1] + heat[k - 2] + heat[k - 2]) / 3;
-  }
-
-  // Step 3.  Randomly ignite new 'sparks' of heat near the bottom
-  if (random8() < SPARKING)
-  {
-    int y = random8(7);
-    heat[y] = qadd8(heat[y], random8(160, 255));
-  }
-
-  // Step 4.  Map from heat cells to LED colors
-  for (int j = 0; j < NUM_LEDS; j++)
-  {
-    CRGB color = HeatColor(heat[j]);
-    int pixelnumber;
-    if (gReverseDirection)
-    {
-      pixelnumber = (NUM_LEDS - 1) - j;
-    }
-    else
-    {
-      pixelnumber = j;
-    }
-    leds[pixelnumber] = color;
-  }
-}
 
 void runLedEffect()
 {
+  // Serial.println(currentMode);
   switch (currentMode)
   {
   case '0':
     static uint8_t startIndex = 0;
-    startIndex = startIndex + 1; /* motion speed */
+    // startIndex = startIndex + 1; /* motion speed */
     FillLEDsFromPaletteColors(startIndex);
     break;
   case '1':
     FillLEDsWithSolidColor();
-    break;
-  case '2':
-    runActiveEffect();
     break;
   default:
     Serial.println("Got invalid mode (in runLedEffect)");
