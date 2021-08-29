85779757942c8246fe2c31f0008a26b302c08f53
Aapo Rista <aapris@gmail.com>
2021-08-29 18:20:26 +0300

    Update examples

--- WeatherLamp/settings-example.h
+++ WeatherLamp/settings-example.h
@@ -1,14 +1,6 @@
-// MQTT settings
-#define MQTT_SERVER "mqtt.example.org"
-#define MQTT_PORT 1883
-#define MQTT_USER "mqtt_user_with_read-write_permission_to_topic"
-#define MQTT_PASSWORD "mqtt_password"
-#define MQTT_SUB_TOPIC  "led/control"
-#define MQTT_PUB_TOPIC  "led/ping"
-
-#define AP_NAME  "PikkuJoulu"
-#define NUM_LEDS 6
-#define NUM_LEDS    10
+#define AP_NAME  "WeatherLamp"
+#define USER_AGENT  "WeatherLamp/0.1.0"
+// #define NUM_LEDS 16
 #ifndef FastLED
   #include <FastLED.h>
 #endif
@@ -16,6 +8,7 @@
 #define CLK_PIN     D5
 #define BRIGHTNESS  150
 #define LED_TYPE    WS2812B
+// #define LED_TYPE    WS2812B
 // #define LED_TYPE    LPD8806
 #define COLOR_ORDER GRB
 CRGB leds[NUM_LEDS];
